dc39b7b0d79ae47a3a6cd277d6b2f7cbf8158aae
llbbllbb2000 <zisheng.cai2000@gmail.com>
2023-06-28 10:48:33 -0400

    finish ctarget part

---
+++ target1/ctarget3.d
@@ -0,0 +1,11 @@
+
+ctarget3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 78 dc 61 55 	mov    $0x5561dc78,%rdi
+   7:	48 89 fc             	mov    %rdi,%rsp
+   a:	68 fa 18 40 00       	push   $0x4018fa
+   f:	c3                   	ret    
---
+++ target1/ctarget3.s
@@ -0,0 +1,4 @@
+movq $0x5561dc78, %rdi
+movq %rdi, %rsp
+pushq $0x4018fa
+ret
